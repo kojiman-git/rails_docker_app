189a84184c26279a9d2ecf316fde21208eb7665c
kojiman-git <kojitatu3@gmail.com>
2025-01-22 00:46:00 +0900

    Redis上でResqueが管理している「キュー」の 一覧をビューに表示するよう実装

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -1,6 +1,19 @@
 class SampleController < ApplicationController
-  def index
+  def enqueue
     Resque.enqueue(SampleJob, "World")
     render plain: "Job has been enqueued!"
+  end
+
+  def index
+    # Resqueのキューとジョブの状態を取得
+    @queues = Resque.queues.map do |queue_name|
+      {
+        name: queue_name,
+        size: Resque.size(queue_name),
+        processed: Resque::Stat[:processed],
+        failed: Resque::Stat[:failed],
+        pending: Resque.info[:pending]
+      }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :sample
+  get 'enqueue', to: 'sample#enqueue'
+  get 'sample', to: 'sample#index'
 end
